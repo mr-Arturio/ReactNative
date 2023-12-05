1a046061ae8fe5cef50fff7bb139b387f24df0f6
ArturT <mr.arturio@gmail.com>
2023-12-05 15:30:00 -0500

    some basic manipulations

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,22 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, Image } from "react-native";
 
 export default function App() {
-  console.log("App executed!");
-
   return (
-    <View style={styles.container}>
-      <Text>Test test finaly!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1}>
+        {" "}
+        Laboris proident id id aute qui nulla culpa esse reprehenderit commodo
+        veniam do est ea. Nisi aliquip eu labore eu magna nostrud velit duis ex
+        magna adipisicing. Ullamco anim incididunt voluptate amet aliquip
+        deserunt duis deserunt sint fugiat cillum qui. Sunt ea amet occaecat
+        aute elit.{" "}
+      </Text>
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
