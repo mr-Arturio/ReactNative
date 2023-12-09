046875305fafbc33363684c68fd0445e9a0d8ee0
ArturT <mr.arturio@gmail.com>
2023-12-08 23:03:26 -0500

    add navigation and cofig folders

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -3,6 +3,6 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen ";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
   
 }
